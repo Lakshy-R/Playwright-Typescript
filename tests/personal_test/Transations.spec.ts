90e1dd95ad67715f9d1ff1380205497dd1254426
Lakshy Rupani <lakshyrupani.lr@gmail.com>
2025-07-29 14:27:11 +0300

    More Tests

---
+++ tests/personal_test/Transations.spec.ts
@@ -0,0 +1,52 @@
+import {test, expect} from '@playwright/test';
+import transations from './transations.json' assert { type: 'json' };
+import transationsCard from './transationsCard.json' assert { type: 'json' };
+import emptyTransations from './emptyTransations.json' assert { type: 'json' };
+
+
+test.describe('Check transation', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://lib-app-c921f.web.app/auth/login');
+        await page.getByPlaceholder('Enter your username').fill('Melgazzar');
+        await page.getByPlaceholder('Enter Password').fill('P@ssw0rd1234');
+        await page.click('div.is_Button._bg-0hover-c-primary6048._bg-0disabled-c-secondary50547._bg-0active-c-primary9548._dsp-flex._fb-auto._bxs-border-box._pos-relative._mih-0px._miw-0px._fs-0._fd-row._jc-center._ai-center._btlr-t-radius-m._btrr-t-radius-m._bbrr-t-radius-m._bblr-t-radius-m._pt-t-space-m._pb-t-space-m._bg-c-primary8048._btc-c-transpare3526._brc-c-transpare3526._bbc-c-transpare3526._blc-c-transpare3526._cur-pointer._gap-t-space-xs._mt-t-space-m');
+        await page.waitForNavigation(); 
+        await page.waitForLoadState('networkidle')
+        
+        await page.route('*/**/api/v1/agb/cards', async (route) => {
+            const json = transationsCard;
+            await route.fulfill({ json });
+        });
+        
+    });
+    
+    test('Bilingual - Check that transactions are retrieved successfully ', async ({ page }) => {
+        await page.route('*/**/api/v1/agb/cards', async (route) => {
+            const json = transationsCard;
+            await route.fulfill({ json });
+        });
+        await page.route('*/**/api/v1/agb/cards/transactions/100000189902', async (route) => {
+            const json = transations;
+            await route.fulfill({ json });
+            
+        });
+        
+        await page.getByText('Show All').click();
+        await expect(page.getByText('Transaction History')).toBeVisible();
+        await expect(page.getByText('95840247')).toBeVisible();
+    
+    })
+
+    test('Empty Transations ', async ({ page }) => {
+        await page.route('*/**/api/v1/agb/cards/transactions/100000189902', async (route) => {
+            const json = emptyTransations;
+            await route.fulfill({ json });
+        });
+        await page.getByText('Show All').click();
+        await expect(page.getByText('Transaction History')).toBeVisible();
+        await expect(page.getByText('No transaction found')).toBeVisible();
+    
+    })
+
+
+})
